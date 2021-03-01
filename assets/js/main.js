8d89ca4d62471ab726b58a3d86672eddb1ce70b7
icsces <icsces@users.noreply.github.com>
2021-03-01 10:51:03 +0900

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,7 +54,7 @@ $(document).ready(function() {
 
 	/* ======= Countdown ========= */
 	// set the date we're counting down to
-    var target_date = new Date("March 05, 2021").getTime();
+    var target_date = new Date("May 15, 2021").getTime();
 
     // variables for time units
     var days, hours, minutes, seconds;
